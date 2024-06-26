06da5be5b7e98039cae7cf83fe416f8a9c1c1e71
DESKTOP-V8DLB94\kobay <kobayashiryu123@gmail.com>
2024-06-26 16:09:24 +0900

    ビルド対応

--- src/DefeatYourOpponent.UI/Program.cs
+++ src/DefeatYourOpponent.UI/Program.cs
@@ -2,12 +2,19 @@ using DefeatYourOpponent.Domain;
 using DefeatYourOpponent.Infrastructure;
 using DefeatYourOpponent.UI.Components;
 using DefeatYourOpponent.UI.ViewModel;
+using Microsoft.AspNetCore.HttpOverrides;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.KnownProxies.Add(IPAddress.Parse("10.0.0.100"));
+});
 
 Shared.SettingEntity = Factories.CreateSettingFileRepository().GetEntity();
 
@@ -21,6 +28,8 @@ builder.Services.AddSingleton(
 builder.Services.AddScoped<MatchIndexViewModel>();
 builder.Services.AddTransient<MatchDetailViewModel>();
 
+builder.Services.AddAuthentication();
+
 var app = builder.Build();
 
 
@@ -28,14 +37,20 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     app.UseHsts();
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.UseAuthentication();
 
 app.Run();
